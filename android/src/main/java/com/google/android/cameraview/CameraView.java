b3deeaecbdb8019497cf51b5ae532f464b7a6d39
Oleksandr Skachkov <gskachkov@gmail.com>
2024-08-05 11:07:31 +0300

    Update

--- android/src/main/java/com/google/android/cameraview/CameraView.java
+++ android/src/main/java/com/google/android/cameraview/CameraView.java
@@ -151,7 +151,7 @@ public class CameraView extends FrameLayout {
         }
     
         Log.e(TAG, "Build.VERSION.SDK_IN:" + Build.VERSION.SDK_INT);
-        if (Build.VERSION.SDK_INT < 21) {
+        if (fallbackToOldApi || Build.VERSION.SDK_INT < 21) {
             mImpl = new Camera1(mCallbacks, preview, mBgHandler);
         } else if (Build.VERSION.SDK_INT < 23) {
             Log.e(TAG, "Step-0-1");
